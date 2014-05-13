e2283fb64420fab7ff70b3b24e3ec8c0c7bc36b7
Vahan Hartooni <vahan.hartooni@codazen.com>
2014-05-13 16:55:11 -0700

    Make captureWebsite return a promise. For now the script does nothing.

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var path = require('path');
 var childProcess = require('child_process');
 var fs = require('fs');
 var phantomjs = require('phantomjs');
+var Promise = require('es6-promise').Promise;
 var binPath = phantomjs.path;
 
 // List of resolutions our client would like to see
@@ -20,62 +21,35 @@ var resolutions = [
 ];
 
 // Calling this function is the same as calling our command to
-// generate a screenshot 
-function captureWebsite (url, width, height, output, callback) {
-  var resolution = width + 'x' + height;
+// generate a screenshot except that it returns a promise
+function captureWebsite (url, width, height, output) {
+  return new Promise(function captureWebsite (resolve) {
+    var resolution = width + 'x' + height;
 
-  console.log('Capturing resolution: ' + resolution);
+    console.log('Capturing resolution: ' + resolution);
 
-  var childArgs = [
+    var childArgs = [
 
-    // Specify the PhantomJS script to run
-    path.join(__dirname, 'screenshot.js'),
+      // Specify the PhantomJS script to run
+      path.join(__dirname, 'screenshot.js'),
 
-    // Specify the arguments
-    url, width, height, output
-  ];
+      // Specify the arguments
+      url, width, height, output
+    ];
 
-  // Calls the PhantomJS command to capture the website
-  var produceScreenshot = childProcess.spawn(binPath, childArgs);
+    // Calls the PhantomJS command to capture the website
+    var produceScreenshot = childProcess.spawn(binPath, childArgs);
 
-  // The next two event listeners are here because the PhantomJS process can't
-  // print to screen without node piping it's log to screen
-  produceScreenshot.stderr.on('data', function (data) {
-    console.log(resolution + ' stderr: ' + data);
+    // The next two event listeners are here because the PhantomJS process can't
+    // print to screen without node piping it's log to screen
+    produceScreenshot.stderr.on('data', function (data) {
+      console.log(resolution + ' stderr: ' + data);
+    });
+
+    produceScreenshot.on('close', function (code) {
+      console.log('Finished capturing: ' + resolution + ' with exit code '+code);
+      resolve();
+    });
   });
-
-  produceScreenshot.on('close', function (code) {
-    console.log('Finished capturing: ' + resolution + ' with exit code '+code);
-    callback();
-  });
 }
 
-// Create a folder to save my screenshots in
-if(!fs.existsSync('screenshots')) {
-  fs.mkdirSync('screenshots');
-}
-process.chdir('screenshots');
-
-function captureResolutions (resolutionNumber) {
-
-  // Check if we've screenshot all the resolutions
-  if(resolutionNumber >= resolutions.length) {
-    return 0;
-  }
-
-  var resolution = resolutions[resolutionNumber],
-      width = resolution.width,
-      height = resolution.height,
-      nextResolutionNumber = resolutionNumber + 1;
-  
-  // This function takes url, width, height, the filename of the screenshot
-  // image, and the function to call when the screenshot is done
-  captureWebsite('http://www.html5zombo.com',
-                 width,
-                 height,
-                 'screenshot' + resolutionNumber + '.png',
-                 function callNextResolution () {
-                   captureResolutions(nextResolutionNumber);
-                 });
-}
-captureResolutions(0);
