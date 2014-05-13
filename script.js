435360a5654d041fae0ee6ad0083e09652bd94a1
Vahan Hartooni <vahan.hartooni@codazen.com>
2014-05-13 14:17:24 -0700

    Reduce down our resolutions into a chain of promises

--- script.js
+++ script.js
@@ -3,7 +3,47 @@ var path = require('path');
 var childProcess = require('child_process');
 var fs = require('fs');
 var phantomjs = require('phantomjs');
+var Promise = require('es6-promise').Promise;
 var binPath = phantomjs.path;
+
+// Calling this function is the same as calling our command to
+// generate a screenshot 
+function captureWebsite (url, width, height, output) {
+  return new Promise(function captureWebsite (resolve) {
+    var resolution = width + 'x' + height;
+
+    console.log('Capturing resolution: ' + resolution);
+
+    var childArgs = [
+
+      // Specify the PhantomJS script to run
+      path.join(__dirname, 'screenshot.js'),
+
+      // Specify the arguments
+      url, width, height, output
+    ];
+
+    // Calls the PhantomJS command to capture the website
+    var produceScreenshot = childProcess.spawn(binPath, childArgs);
+
+    // The next two event listeners are here because the PhantomJS process can't
+    // print to screen without node piping it's log to screen
+    produceScreenshot.stderr.on('data', function (data) {
+      console.log(resolution + ' stderr: ' + data);
+    });
+
+    produceScreenshot.on('close', function (code) {
+      console.log('Finished capturing: ' + resolution + ' with exit code '+code);
+      resolve();
+    });
+  });
+}
+
+// Create a folder to save my screenshots in
+if(!fs.existsSync('screenshots')) {
+  fs.mkdirSync('screenshots');
+}
+process.chdir('screenshots');
 
 // List of resolutions our client would like to see
 var resolutions = [
@@ -17,65 +57,24 @@ var resolutions = [
   {width: 960, height: 540},
   {width: 1200, height: 960},
   {width: 1600, height: 1200}
-];
+],
+  screenshotNumber = 0;
 
-// Calling this function is the same as calling our command to
-// generate a screenshot 
-function captureWebsite (url, width, height, output, callback) {
-  var resolution = width + 'x' + height;
+resolutions.reduce(function captureResolutions (sequence, resolution) {
 
-  console.log('Capturing resolution: ' + resolution);
+  // Return my sequence of promises
+  return sequence.then(function captureResolution () {
+    var width = resolution.width,
+        height = resolution.height;
+    
+    screenshotNumber++;
 
-  var childArgs = [
-
-    // Specify the PhantomJS script to run
-    path.join(__dirname, 'screenshot.js'),
-
-    // Specify the arguments
-    url, width, height, output
-  ];
-
-  // Calls the PhantomJS command to capture the website
-  var produceScreenshot = childProcess.spawn(binPath, childArgs);
-
-  // The next two event listeners are here because the PhantomJS process can't
-  // print to screen without node piping it's log to screen
-  produceScreenshot.stderr.on('data', function (data) {
-    console.log(resolution + ' stderr: ' + data);
+    // Return the individual promise
+    return captureWebsite('http://www.html5zombo.com',
+                          width,
+                          height,
+                          'screenshot' + screenshotNumber + '.png');
   });
 
-  produceScreenshot.on('close', function (code) {
-    console.log('Finished capturing: ' + resolution + ' with exit code '+code);
-    callback();
-  });
-}
-
-// Create a folder to save my screenshots in
-if(!fs.existsSync('screenshots')) {
-  fs.mkdirSync('screenshots');
-}
-process.chdir('screenshots');
-
-function captureResolutions (resolutionNumber) {
-
-  // Check if we've screenshot all the resolutions
-  if(resolutionNumber >= resolutions.length) {
-    return 0;
-  }
-
-  var resolution = resolutions[resolutionNumber],
-      width = resolution.width,
-      height = resolution.height,
-      nextResolutionNumber = resolutionNumber + 1;
-  
-  // This function takes url, width, height, the filename of the screenshot
-  // image, and the function to call when the screenshot is done
-  captureWebsite('http://www.html5zombo.com',
-                 width,
-                 height,
-                 'screenshot' + resolutionNumber + '.png',
-                 function callNextResolution () {
-                   captureResolutions(nextResolutionNumber);
-                 });
-}
-captureResolutions(0);
+   // Our first promise, already resolved, to start off our sequence of promises
+}, Promise.resolve());
